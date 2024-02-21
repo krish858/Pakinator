5bc68290b5840ad6baaf4764ae36835b613d5e70
krish858 <unknowmortal57@gmail.com>
2024-02-22 02:22:46 +0530

    started code

--- src/components/Password_Genarator.jsx
+++ src/components/Password_Genarator.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState,useCallback } from 'react'
 
 function Password_Genarator() {
+    const [length,setlength] = useState(8);
+    const [Numberallowed,setNumberallowed] = useState(false);
+    const [Charallowed,setCharallowed] = useState(true);
+    const [Passwd,setPasswd] = useState("")
+
+    const passwdgen = useCallback(() => {
+        let pass = ""
+        let str = "qwertyuiopasdfghjklzxcvbnmZXCVBNMLKJHGFDSAQWERTYUIOP"
+        if ()
+    },[length,Numberallowed,Charallowed,setPasswd])
+
   return (
     <div>Password_Genarator</div>
   )
