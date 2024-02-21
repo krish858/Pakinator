e9f9491514b142b2b54c1a63a248f3d6f1944c20
krish858 <unknowmortal57@gmail.com>
2024-02-22 02:02:00 +0530

    Added a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Head from "./components/Head"
+import Password_Genarator from "./components/Password_Genarator"
 function App() {
 
   return (
     <>
         <Head/>
+        <Password_Genarator/>
     </>
   )
 }
---
+++ src/components/Password_Genarator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Password_Genarator() {
+  return (
+    <div>Password_Genarator</div>
+  )
+}
+
+export default Password_Genarator
