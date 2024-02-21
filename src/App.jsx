13453c335815ffb91b472d68c15e305b54cc0d17
krish858 <unknowmortal57@gmail.com>
2024-02-22 01:46:32 +0530

    starting phase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Head from "./components/Head"
+function App() {
+
+  return (
+    <>
+        <Head/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div>
+        <h1 className="text-3xl text-[#ff4500] text-center pt-10">PAKINATOR</h1>
+        <p className="text-white text-xl text-center">The Password Generator</p>
+    </div>
+  )
+}
+
+export default Head
